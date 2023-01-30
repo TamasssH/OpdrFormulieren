bccf922d261e5483d66eba9df3ec3e33f81ed9d5
TamasH <unconfigured@null.spigotmc.org>
2023-01-30 12:23:53 +0100

    check voor talen

--- index.php
+++ index.php
@@ -12,10 +12,14 @@
 
     Naam:<input type="text" name="naam" placeholder="Uw naam" required /><br />
 
-    Land:<select name="land"> 
+    Land:<select name="land">
+            <option value="" selected>Maak uw Keuze: </option> 
             <option value="NL">Nederland</option> 
             <option value="DE">Duitsland</option> 
             <option value="EN">Engeland</option>
+            <option value="Fr">Frans</option>
+            <option value="Es">Spaans</option>
+            <option value="It">Italiaans</option>
          </select>
 
         <br />
@@ -26,15 +30,28 @@
     if(isset($_POST["submit"])){
         $naam = $_POST["naam"];
         $land = $_POST["land"];
-    if($land=="NL"){
-        echo "Goedemorgen ".$naam;
- }
-    elseif($land=="DE"){
-        echo "Guten Morgen ".$naam;
- }
-    elseif($land=="EN"){
-        echo "Good Morning ".$naam;
-      }
+
+        if($land==""){
+            echo "Weet je zeker dat je het wilt versturen zonder taal?";
+        }
+        if($land=="NL"){
+            echo "Goedemorgen ".$naam;
+        }
+        elseif($land=="DE"){
+            echo "Guten Morgen ".$naam;
+        }
+        elseif($land=="EN"){
+            echo "Good Morning ".$naam;
+        }
+        elseif($land=="Fr"){
+            echo "bonjour ".$naam;
+        }
+        elseif($land=="Es"){
+            echo "Buenos días ".$naam;
+        }
+        elseif($land=="It"){
+            echo "Buongiorno ".$naam;
+    }
  }
 ?>
 
